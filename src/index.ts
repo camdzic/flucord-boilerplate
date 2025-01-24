b096d3bc704292aed39fde8e8af844b7bb399d1c
Aldin Čamdžić <acamdzic0@proton.me>
2025-01-25 00:29:24 +0100

    refactor(Flucord); simplification

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,15 @@ import { PrismaClient } from "@prisma/client";
 import { Partials } from "discord.js";
 import { Flucord } from "flucord";
 
-class ExtendedFlucord extends Flucord {
-  constructor() {
-    super({
-      intents: [],
-      partials: [
-        Partials.Channel,
-        Partials.Message,
-        Partials.User,
-        Partials.GuildMember,
-        Partials.Reaction
-      ]
-    });
+const flucord = new Flucord({
+  intents: [],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+    Partials.GuildMember,
+    Partials.Reaction
+  ]
+});
 
-    this.prisma = new PrismaClient();
-  }
-}
-
-new ExtendedFlucord();
+flucord.prisma = new PrismaClient();
