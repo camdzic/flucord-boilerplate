14c05d45d495ffb32c722d446ecbd89a86fa3467
Aldin Čamdžić <acamdzic0@proton.me>
2025-02-02 00:29:16 +0100

    feat(cooldown): implement CooldownGuard for rate limiting commands

--- src/commands/slash/general/PingSlashCommand.ts
+++ src/commands/slash/general/PingSlashCommand.ts
@@ -1,5 +1,15 @@
-import type { CacheType, ChatInputCommandInteraction } from "discord.js";
-import { BaseSlashCommand, type Flucord } from "flucord";
+import {
+  type CacheType,
+  ChannelType,
+  type ChatInputCommandInteraction
+} from "discord.js";
+import {
+  BaseSlashCommand,
+  ChannelTypeGuard,
+  type Flucord,
+  Time
+} from "flucord";
+import { CooldownGuard } from "../../../guards/CooldownGuard";
 
 export class PingSlashCommand extends BaseSlashCommand {
   constructor(flucord: Flucord) {
@@ -7,7 +17,34 @@ export class PingSlashCommand extends BaseSlashCommand {
       name: "ping",
       description: "Pong!",
       category: "general",
-      guildPlusUser: true
+      guildPlusUser: true,
+      guards: [
+        new ChannelTypeGuard(
+          ChannelType.GuildText,
+          ChannelType.DM,
+          ChannelType.GroupDM
+        ),
+        new CooldownGuard(
+          interaction => {
+            // If the user has nitro, the cooldown is 30 seconds
+            if (interaction.member.premiumSince) {
+              return Time.Second * 30;
+            }
+
+            // Otherwise, the cooldown is 1 minute
+            return Time.Minute;
+          },
+          interaction => {
+            if (interaction.member.premiumSince) {
+              // If the user has nitro, the threshold is 3 uses
+              return 3;
+            }
+
+            // Otherwise, the threshold is 1 use
+            return 1;
+          }
+        )
+      ]
     });
   }
 
---
+++ src/guards/CooldownGuard.ts
@@ -0,0 +1,127 @@
+import { RateLimitManager } from "@sapphire/ratelimits";
+import type {
+  CacheType,
+  ChatInputCommandInteraction,
+  MessageContextMenuCommandInteraction,
+  UserContextMenuCommandInteraction
+} from "discord.js";
+import {
+  BaseGuard,
+  CooldownGuardException,
+  GuardExecutionFailException,
+  Time
+} from "flucord";
+
+type CooldownResolver = (
+  interaction:
+    | ChatInputCommandInteraction<"cached">
+    | MessageContextMenuCommandInteraction<"cached">
+    | UserContextMenuCommandInteraction<"cached">
+) => number;
+
+export class CooldownGuard extends BaseGuard<
+  "slashCommand" | "messageContextMenuCommand" | "userContextMenuCommand"
+> {
+  private readonly rateLimitManagers: Map<string, RateLimitManager>;
+
+  private time: CooldownResolver | number;
+  private threshold: CooldownResolver | number;
+
+  constructor(
+    time: CooldownResolver | number,
+    threshold: CooldownResolver | number = 1
+  ) {
+    super({
+      types: [
+        "slashCommand",
+        "messageContextMenuCommand",
+        "userContextMenuCommand"
+      ]
+    });
+
+    this.rateLimitManagers = new Map();
+
+    this.time = time;
+    this.threshold = threshold;
+  }
+
+  execute(
+    interaction:
+      | ChatInputCommandInteraction<CacheType>
+      | MessageContextMenuCommandInteraction<CacheType>
+      | UserContextMenuCommandInteraction<CacheType>
+  ) {
+    if (!interaction.inCachedGuild()) {
+      throw new GuardExecutionFailException(
+        "While executing CooldownGuard, guild was not found"
+      );
+    }
+
+    const realTime = this.resolveValue(this.time, interaction);
+    const realThreshold = this.resolveValue(this.threshold, interaction);
+
+    if (interaction.member.permissions.has("ManageGuild")) {
+      const rateLimitManager = this.getRateLimitManager(
+        interaction.commandId,
+        realTime,
+        realThreshold
+      );
+
+      const rateLimit = rateLimitManager.acquire(
+        `${interaction.guild.id}:${interaction.user.id}`
+      );
+      if (rateLimit.limited) {
+        throw new CooldownGuardException(
+          `You can use this command again in \`${this.formatCooldownTime(
+            rateLimit.remainingTime
+          )}\``
+        );
+      }
+
+      rateLimit.consume();
+    }
+  }
+
+  private resolveValue(
+    source: CooldownResolver | number,
+    interaction:
+      | ChatInputCommandInteraction<"cached">
+      | MessageContextMenuCommandInteraction<"cached">
+      | UserContextMenuCommandInteraction<"cached">
+  ) {
+    return typeof source === "function" ? source(interaction) : source;
+  }
+
+  private getRateLimitManager(
+    commandId: string,
+    interval: number,
+    limit: number
+  ) {
+    const existingManager = this.rateLimitManagers.get(commandId);
+    if (existingManager) {
+      return existingManager;
+    }
+
+    const newManager = new RateLimitManager(interval, limit);
+    this.rateLimitManagers.set(commandId, newManager);
+    return newManager;
+  }
+
+  private formatCooldownTime(remainingTime: number) {
+    const units = [
+      { label: "d", value: Time.Day },
+      { label: "h", value: Time.Hour },
+      { label: "m", value: Time.Minute },
+      { label: "s", value: Time.Second }
+    ];
+
+    for (const { label, value } of units) {
+      const time = remainingTime / value;
+      if (time >= 1) {
+        return `${time.toFixed(2)}${label}`;
+      }
+    }
+
+    return `${remainingTime}ms`;
+  }
+}
